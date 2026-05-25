b897d6f98e254c8f419bbf87921303df7897ad4b
jakubmich97-droid <jakubmich97@gmail.com>
2026-05-25 13:57:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://nqvpxopsiiagemumfbmc.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xdnB4b3BzaWlhZ2VtdW1mYm1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2OTQwNTcsImV4cCI6MjA5NTI3MDA1N30.VQYWGLALTxD84EksKwwUuVh5zfoAkCgenhMRXm3xdMs";
+const SUPABASE_URL = "";
+const SUPABASE_ANON_KEY = "";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
